167709cbf2937f036071c837efc71390f61b7d28
Andres Felipe Estupiñan <afestupinanp@unadvirtual.edu.co>
2021-10-07 22:28:32 -0500

    Se agrega botón de responsive
    + Se agrega botón de responsividad para los celulares.

--- fase3/index.js
+++ fase3/index.js
@@ -7,6 +7,9 @@
 
 const dropdowns = ".dropdown";
 const iniciadorDropdown = ".dropdown-iniciador";
+const seccionAnio = "#anio";
+const btnDespliegue = ".seccion-responsive-iniciador";
+const seccionResponsive = ".seccion-responsive";
 
 /**
  * Función que inicializa los componentes normalmente usados en la página.
@@ -21,6 +24,14 @@ const iniciarComponentes = () => {
             elemento.querySelector("ul").classList.add("display-none");
         });
     });
+
+    document.querySelector(btnDespliegue).addEventListener("click", function () {
+        document.querySelector(seccionResponsive).classList.toggle("display-none-celulares");
+    });
+
+    const fecha = new Date();
+    document.querySelector(seccionAnio).innerHTML = `&copy; ${fecha.getFullYear()}`;
+
 }
 
 window.addEventListener("load", iniciarComponentes);
